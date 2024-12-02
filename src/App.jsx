3a1f06400965d0f09217b869e06b37516b3c13e7
Carlo Elvisi <carloelvisi01@outlook.it>
2024-12-02 16:58:53 +0100

    remove tag body

--- src/App.jsx
+++ src/App.jsx
@@ -6,24 +6,21 @@ import Footer from "./components/footer";
 function App() {
   return (
     <>
-      {" "}
-      <body id="body">
-        {/* Header */}
-        <Header />
+      {/* Header */}
+      <Header />
 
-        {/* Titolo del blog */}
-        <h1 className="text-center p-3">Il mio blog</h1>
-        <main>
-          {/* Card centrata */}
-          <div className="d-flex justify-content-center mt-2">
-            <Card />
-          </div>
-        </main>
-        {/* Footer */}
-        <footer id="footer">
-          <Footer />
-        </footer>
-      </body>
+      {/* Titolo del blog */}
+      <h1 className="text-center p-3">Il mio blog</h1>
+      <main>
+        {/* Card centrata */}
+        <div className="d-flex justify-content-center mt-2">
+          <Card />
+        </div>
+      </main>
+      {/* Footer */}
+      <footer id="footer">
+        <Footer />
+      </footer>
     </>
   );
 }
